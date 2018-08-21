355fa4cd38ee96fb0bc6a8924362929202dcae19
Michael Johansson <Michael.varberg@gmail.com>
2018-08-21 21:10:11 +0200

    starting with the decompressclass

--- decompress_files.py
+++ decompress_files.py
@@ -1,5 +1,28 @@
+import zipfile
+import os
+import shutil
 
+#"F:\Downloads\18VT-1DV701-7,5hp-Växjö-50%-Assignment 3 TFTP Server-1755230.zip"
 
+def decompress_one(filename, root_directory):
 
+    save_directory = root_directory + "\\deflated_files\\"
+    if os.path.exists(save_directory):
+        print(save_directory)
+        shutil.rmtree(save_directory)
 
-def decompress_one(filename, save_directory):
+    os.makedirs(save_directory)
+
+    with zipfile.ZipFile(filename, "r") as zip_ref:
+        zip_ref.extractall(save_directory)
+
+    return save_directory
+
+def decompress_assignments(assignment_directory):
+    for root, dirs, files in os.walk(assignment_directory):
+        for file in files:
+            if file.endswith(".zip"):
+                file_path = os.path.join(root,file)
+                with zipfile.ZipFile(file_path, "r") as zip_ref:
+                    zip_ref.extractall(root)
+                os.remove(file_path)
--- plag_finder.py
+++ plag_finder.py
@@ -2,7 +2,16 @@ import os
 import decompress_files
 import run_jplag
 
+
+
 def __main__():
     print("hi")
-    relative_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
+    relative_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),"."))
     print(relative_dir)
+    filename = input("Input absolute path of downloaded zip archive from moodle: ")
+    print(filename)
+    assignment_directory = decompress_files.decompress_one(filename, relative_dir)
+    decompress_files.decompress_assignments(assignment_directory)
+
+
+__main__()
