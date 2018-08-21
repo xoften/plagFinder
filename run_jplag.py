fdc1992ffde0359d172b2a03629aaf08141c8a1b
Michael Johansson <Michael.varberg@gmail.com>
2018-08-21 23:04:45 +0200

    Working prototype

---
+++ run_jplag.py
@@ -0,0 +1,7 @@
+import subprocess
+import os
+
+JPLAG_FILEPATH = os.path.dirname(__file__) + "\jplag.jar"
+def run_jplag(submission_directory):
+
+    subprocess.call(['java', '-jar', JPLAG_FILEPATH, '-l', 'java17', '-s', '-r', submission_directory])
