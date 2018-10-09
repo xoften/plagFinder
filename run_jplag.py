b37ea4864affd37283c79c9ccb555e468f770de9
root <root@localhost.localdomain>
2018-10-09 10:36:42 +0200

    changed from rar file that needed unrar, to patool which dont need any third-party

--- run_jplag.py
+++ run_jplag.py
@@ -3,6 +3,8 @@ import os
 
 JPLAG_FILEPATH = os.path.dirname(__file__) + "\jplag.jar"
 def run_jplag(submission_directory):
-
-    subprocess.call(['java', '-jar', JPLAG_FILEPATH, '-l', 'java17', '-s',
+    print("RUNING JPLAG")
+    print(submission_directory)
+    subprocess.call(['java', '-jar', JPLAG_FILEPATH, '-l', 'java17', '-s', '-r', os.path.dirname(__file__) + "\result",
                      submission_directory], stderr=subprocess.DEVNULL)
+    print("DONE")
